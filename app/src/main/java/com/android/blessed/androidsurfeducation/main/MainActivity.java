92ba431f5ea7733eba3363b7ddc160759248e528
blessedbyjobs <alrbkv@gmail.com>
2019-08-24 00:41:06 +0300

    5. Реализованы основы главного экрана, добавлена Activity-заглушка для поиска

---
+++ app/src/main/java/com/android/blessed/androidsurfeducation/main/MainActivity.java
@@ -0,0 +1,80 @@
+package com.android.blessed.androidsurfeducation.main;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
+
+import com.android.blessed.androidsurfeducation.R;
+import com.arellomobile.mvp.MvpAppCompatActivity;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends MvpAppCompatActivity {
+    private Toolbar mToolbar;
+    private BottomNavigationView mBottomNavigationView;
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.user_info_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getItemId() == R.id.action_search) {
+            Intent intent = new Intent(this, SearchActivity.class);
+            startActivity(intent);
+        }
+        return true;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_activity);
+
+        mToolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(mToolbar);
+
+        loadFragment(new MemesFragment());
+
+        mBottomNavigationView = findViewById(R.id.bottom_navigation_view);
+        mBottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment fragment = null;
+
+                switch (item.getItemId()) {
+                    case R.id.bottom_navigation_memes:
+                        fragment = new MemesFragment();
+                        break;
+
+                    case R.id.bottom_navigation_add_meme:
+                        fragment = new AddMemeFragment();
+                        break;
+
+                    case R.id.bottom_navigation_profile:
+                        fragment = new ProfileFragment();
+                        break;
+                }
+
+                return loadFragment(fragment);
+            }
+        });
+    }
+
+    private boolean loadFragment(Fragment fragment) {
+        if (fragment != null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.fragment_container, fragment)
+                    .commit();
+            return true;
+        }
+        return false;
+    }
+}
---
+++ app/src/main/java/com/android/blessed/androidsurfeducation/main/SearchActivity.java
@@ -0,0 +1,16 @@
+package com.android.blessed.androidsurfeducation.main;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.android.blessed.androidsurfeducation.R;
+
+public class SearchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.search_activity);
+    }
+}
